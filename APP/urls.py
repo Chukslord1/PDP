9e79e4e183c10049dd10fa2f4f6b93da85de5d92
Chukslord1 <chukslord1@gmail.com>
2020-03-07 14:12:27 -0800

    first

---
+++ APP/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+
+
+]
